41bbbd1f22591b488c111cc638d01ed5f70044bc
zuoqy <zuoqy@ziroom.com>
2019-09-01 15:50:18 +0800

    dist

--- front-end/src/js/request.js
+++ front-end/src/js/request.js
@@ -2,12 +2,12 @@ const superagent = require('superagent')
 const cheerio = require('cheerio');
 
 export  function fetch(params) {
-  return superagent.get('http://192.168.0.106:3000/fetch')
+  return superagent.get('http://47.96.231.177:3000/fetch')
       .query(params)
   
 }
 export function getScore(params) {
-  return superagent.get('http://192.168.0.106:3000/score')
+  return superagent.get('http://47.96.231.177:3000/score')
       .query(params)
 
 
