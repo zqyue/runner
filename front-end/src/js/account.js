ac54ee7dac845c761008b7dcc7898c4b2c3e34c2
zuoqy <zuoqy@ziroom.com>
2020-03-09 16:07:15 +0800

    udpate

---
+++ front-end/src/js/account.js
@@ -0,0 +1,4 @@
+// const fs=require('fs')
+// var data = fs.readFileSync('./count.txt');
+// console.log("同步读取: " + data.toString());
+// fs.writeFile('./count.txt',(data+1).toString(),{flag:'w'},()=>{})
